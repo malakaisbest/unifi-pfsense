34ce0801aaedc1ba66ffded166270abd0ee8f4bb
tong2x <40678733+tong2x@users.noreply.github.com>
2024-05-23 14:52:03 +0800

    8.2.87
    8.2.87

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -4,7 +4,8 @@
 # Installs the Uni-Fi controller software on a FreeBSD machine (presumably running pfSense).
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/8.1.127-810cd1e59a/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/8.2.87-54321d7713/UniFi.unix.zip"
+
 
 # The rc script associated with this branch or fork:
 RC_SCRIPT_URL="https://raw.githubusercontent.com/unofficial-unifi/unifi-pfsense/master/rc.d/unifi.sh"
